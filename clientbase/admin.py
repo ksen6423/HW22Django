25caef8790d485e50d0f1055e740d94dae405976
Kseniia Vakhterova <vlsen23@list.ru>
2026-09-04 12:11:49 +0300

    homework4 fixes2

---
+++ clientbase/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+
+from clientbase.models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ("email",)
+    list_filter = ("email",)
+    search_fields = ("email",)
---
+++ clientbase/models.py
@@ -0,0 +1,43 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(
+        unique=True,
+        verbose_name="Email"
+    )
+    phone_number = models.CharField(
+        max_length=15, verbose_name="Телефон",
+        blank=True,
+        null=True,
+        help_text="Введите номер телефона"
+    )
+    avatar = models.ImageField(
+        upload_to='avatars/',
+        verbose_name="Аватар",
+        blank=True,
+        null=True,
+        help_text="Загрузите свой аватар"
+    )
+    country = models.CharField(
+        max_length=50,
+        blank=True,
+        null=True
+    )
+    token = models.CharField(
+        max_length=100,
+        verbose_name="Token",
+        blank=True,
+        null=True
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.email
